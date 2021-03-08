7bcd479b3662eff99b977b5fe781ef51b8bcb6fc
SebastianLee <heechanlee98@gmail.com>
2021-03-08 11:19:18 -0800

    js working

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var apple = 3;
-console.log(apple);
+class Clothes {
+    constructor(type, color) {
+        this.type = type;
+        this.color = color;
+    }
+}
+
+var jeans = new Clothes("Jeans", "Blue");
+var jacket = new Clothes("Jacket", "Black");
+
+document.getElementById("selection").innerHTML = jeans.color;
+
+console.log(jeans.type);
