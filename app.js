c0131a70e1ade07d7b665e477e5b38d481679413
SebastianLee <heechanlee98@gmail.com>
2021-03-08 12:24:25 -0800

    filter button problems

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 class Clothes {
-    constructor(type, color) {
+    constructor(type, color, show) {
         this.type = type;
         this.color = color;
+        this.show = show;
     }
 }
 
-var jeans = new Clothes("Jeans", "Blue");
-var jacket = new Clothes("Jacket", "Black");
+var jeans = new Clothes("Jeans", "Blue", true);
+var jacket = new Clothes("Jacket", "Black", false);
 
-document.getElementById("selection").innerHTML = jeans.color;
+// Jeans 
+document.getElementById("type-1").innerHTML = jeans.type;
+document.getElementById("color-1").innerHTML = jeans.color;
 
-console.log(jeans.type);
+// Jacket 
+document.getElementById("type-2").innerHTML = jacket.type;
+document.getElementById("color-2").innerHTML = jacket.color;
+
+// Filter Button
+document.getElementById("filter").addEventListener('click', function () {
+    if (jacket.show = false) {
+        var removeJacket = document.getElementById("type-2")
+        removeJacket.remove();
+    } 
+})
